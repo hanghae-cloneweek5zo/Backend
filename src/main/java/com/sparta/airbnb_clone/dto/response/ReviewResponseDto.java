8735b5ed2ee02f9968b64ef7ce8b8a9e1bfabc44
zero <abeb3@naver.com>
2022-08-21 23:30:32 +0900

    feat
    feat : ReviewResponseDto 생성자 생성

--- src/main/java/com/sparta/airbnb_clone/dto/response/ReviewResponseDto.java
+++ src/main/java/com/sparta/airbnb_clone/dto/response/ReviewResponseDto.java
@@ -1,7 +1,13 @@
 package com.sparta.airbnb_clone.dto.response;
 
+import com.sparta.airbnb_clone.domain.Review;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -9,5 +15,16 @@ public class ReviewResponseDto {
     private Long reviewId;
     private String description;
     private int star;
-    private double starAvg;
+    private LocalDateTime createdAt;
+//    private Double starAvg;
+
+    public ReviewResponseDto(Review review){
+        this.reviewId = review.getReviewId();
+        this.description = review.getDescript();
+        this.star = review.getStar();
+        this.createdAt = review.getCreatedAt();
+    }
+
+//    public ReviewResponseDto(Review review, List<Review> reviewList, Long )
+
 }
