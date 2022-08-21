d55e9b3f1472d896c088889d55f3a32e54980e18
zero <abeb3@naver.com>
2022-08-21 23:37:28 +0900

    feat , refactor
    refactor  : 
    ReviewResponseDto 생성자 메소드로 인해 코드 리팩터링
    feat : 리뷰 전체 조회기능, 리뷰 star 평균값 조회 기능 구현

--- src/main/java/com/sparta/airbnb_clone/service/ReviewService.java
+++ src/main/java/com/sparta/airbnb_clone/service/ReviewService.java
@@ -1,6 +1,7 @@
 package com.sparta.airbnb_clone.service;
 
 
+import com.sparta.airbnb_clone.domain.House;
 import com.sparta.airbnb_clone.domain.Review;
 import com.sparta.airbnb_clone.dto.request.ReviewRequestDto;
 import com.sparta.airbnb_clone.dto.response.ResponseDto;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,16 +22,44 @@ public class ReviewService {
 
 
     @Transactional
-    public ResponseDto<?> createReview(ReviewRequestDto requestDto, HttpServletRequest httpServlet, Long Id){
-        if(requestDto.getDescription() == null){
+    public ResponseDto<?> createReview(ReviewRequestDto requestDto, HttpServletRequest httpServlet, Long accId){
+        if(requestDto.getDescript() == null){
             return ResponseDto.fail("DESCRIPTION_NULL","리뷰를 입력해주세요");
         }
-        Review review = new Review(requestDto.getDescription(),requestDto.getStar());
+
+        Review review = new Review(requestDto.getDescript(),requestDto.getStar());
 
         reviewRepository.save(review);
 
-        ReviewResponseDto reviewResponseDto = new ReviewResponseDto(review.getReviewId(),review.getDescription(),review.getStar(),review.getStarAvg());
+        ReviewResponseDto reviewResponseDto = new ReviewResponseDto(review);
 
         return ResponseDto.success(reviewResponseDto);
     }
+
+    @Transactional(readOnly = true)
+    public ResponseDto<?> getReviews(){
+        ArrayList<ReviewResponseDto> reviewResponseDto = new ArrayList<>();
+
+        for(Review review : reviewRepository.findAllBy()){
+            reviewResponseDto.add(new ReviewResponseDto(review));
+        }
+
+        return ResponseDto.success(reviewResponseDto);
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseDto<?> getReviewsStar(Long accId){
+        double reviewStarAvg = this.getRevieStarAvg(accId);
+        return ResponseDto.success(reviewStarAvg);
+    }
+
+    public Double getRevieStarAvg(Long accId){
+        List<Review> reviewList = reviewRepository.findAllByHouseAccommodationIdOrderByCreatedAtDesc(accId);
+        double starAvg = 0;
+        for(Review review : reviewList){
+            starAvg += review.getStar();
+        }
+        return starAvg / reviewList.size();
+    }
+
 }
