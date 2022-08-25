14ffc555df6287793f54c211cf1fa2305ecc71a2
zero <abeb3@naver.com>
2022-08-25 13:49:01 +0900

    feat
    페이징 기능 처리 완료

--- src/main/java/com/sparta/airbnb_clone/repository/support/HouseRepositorySupport.java
+++ src/main/java/com/sparta/airbnb_clone/repository/support/HouseRepositorySupport.java
@@ -2,13 +2,19 @@ package com.sparta.airbnb_clone.repository.support;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.airbnb_clone.domain.House;
 import com.sparta.airbnb_clone.dto.response.HouseMainResponseDto;
 import com.sparta.airbnb_clone.shared.Category;
 import com.sparta.airbnb_clone.shared.FacilityType;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
+
+
 import java.util.List;
 
 import static com.sparta.airbnb_clone.domain.QFacility.facility;
@@ -42,7 +48,8 @@ public class HouseRepositorySupport extends QuerydslRepositorySupport {
                 .where(house.category.eq(category))
                 .fetch();
     }
-    public List<HouseMainResponseDto> findAllByOrderByModifiedAtDesc(){
+
+    public List<HouseMainResponseDto> findAllByOrderByModifiedAtDesc() {
         return queryFactory
                 .select(Projections.fields(
                         HouseMainResponseDto.class,
@@ -59,10 +66,11 @@ public class HouseRepositorySupport extends QuerydslRepositorySupport {
                 .on(house.houseId.eq(houseImg.HouseImgId))
                 .fetch();
     }
-    public List<HouseMainResponseDto> findAllByFilter(int minPrice, int maxPrice,
-                                                      int bedRoomCnt, int bedCnt, List<FacilityType> facilities){
 
-        return queryFactory
+    public PageImpl<HouseMainResponseDto> findAllByFilter(int minPrice, int maxPrice,
+                                                      int bedRoomCnt, int bedCnt, List<FacilityType> facilities, Pageable pageable) {
+
+        List<HouseMainResponseDto> content = queryFactory
                 .select(Projections.fields(
                         HouseMainResponseDto.class,
                         house.houseId,
@@ -82,29 +90,32 @@ public class HouseRepositorySupport extends QuerydslRepositorySupport {
                         eqBedRoomCnt(bedRoomCnt),
                         eqBedCnt(bedCnt),
                         eqFacilities(facilities))
-                .orderBy(house.createdAt.desc())
-//                .offset(pageable.getOffset())
-//                .limit(pageable.getPagesize)
+                .groupBy(house.houseId)
+                .orderBy(house.houseId.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
                 .fetch();
+
+        return new PageImpl<>(content,pageable,content.size());
     }
 
     private BooleanExpression betweenPrice(int minPrice, int maxPrice) {
-        if(minPrice == 0 && maxPrice == 0) return null;
+        if (minPrice == 0 && maxPrice == 0) return null;
         return house.price.between(minPrice, maxPrice);
     }
 
-    private BooleanExpression eqBedRoomCnt(int bedRoomCnt){
-        if(bedRoomCnt == 0)  return null;
+    private BooleanExpression eqBedRoomCnt(int bedRoomCnt) {
+        if (bedRoomCnt == 0) return null;
         return house.bedRoomCnt.eq(bedRoomCnt);
     }
 
     private BooleanExpression eqBedCnt(int bedCnt) {
-        if(bedCnt == 0)  return null;
+        if (bedCnt == 0) return null;
         return house.bedRoomCnt.eq(bedCnt);
     }
 
-    private BooleanExpression eqFacilities(List<FacilityType> facilities){
-        if(facilities.isEmpty()) return null;
+    private BooleanExpression eqFacilities(List<FacilityType> facilities) {
+        if (facilities.isEmpty()) return null;
         return facility.facilityType.in(facilities);
     }
 }
